0b10f75b2324b908a2798bbd14558597558da1d8
Sandro Guerreiro <47953373+SandroGuerreiro@users.noreply.github.com>
2023-10-27 15:09:39 +0200

    Nvim tree configs and alacritty

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -4,15 +4,35 @@ return {
   lazy = false,
   dependencies = {
     "nvim-tree/nvim-web-devicons",
+    {
+      "JMarkin/nvim-tree.lua-float-preview",
+      lazy = true,
+    }
   },
   config = function()
     require("nvim-tree").setup{
-      view = {
-        width = 30,
+      update_cwd = true,
+      actions = {
+        open_file = {
+          resize_window = true,
+        },
       },
-      -- git = {
-      --  enable = false,
-      -- },
+      view = {
+        side = "left",
+      },
+      update_focused_file = {
+        enable = true,
+        update_cwd = true,
+      },
+      filters = {
+        custom = { "^\\.git" }
+      },      
+      diagnostics = {
+        enable = true,
+      },
+      renderer = {
+        highlight_modified = "icon",
+      }
     }
-  end,
+  end
 }
