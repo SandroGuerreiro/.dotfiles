fc0ba2e4405611e6be5098820b82ec4f48ae497c
Sandro Guerreiro <47953373+SandroGuerreiro@users.noreply.github.com>
2023-11-17 15:47:26 +0100

    Allow the setup to be executed in any directory

--- setup.sh
+++ setup.sh
@@ -1,15 +1,19 @@
-alacritty_setup="alacritty/setup.sh"
+#!/bin/bash
+
+base_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/"
+
+alacritty_setup="${base_path}alacritty/setup.sh"
 chmod +x "$alacritty_setup"
 $alacritty_setup
 
-git_setup="./git/setup.sh"
+git_setup="${base_path}./git/setup.sh"
 chmod +x "$git_setup"
 $git_setup
 
-nvim_setup="./nvim/setup.sh"
+nvim_setup="${base_path}./nvim/setup.sh"
 chmod +x "$nvim_setup"
 $nvim_setup
 
-tmux_setup="./tmux/setup.sh"
+tmux_setup="${base_path}./tmux/setup.sh"
 chmod +x "$tmux_setup"
 $tmux_setup
