c65725ac19bb5d83abf1d104f9091e4e660624f6
Sandro Guerreiro <47953373+SandroGuerreiro@users.noreply.github.com>
2024-01-05 11:17:13 +0000

    Add bars to nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,6 +3,3 @@ require('autocmds')
 require('plugin_list')
 require('keymaps')
 require('lsp')
-
-
-vim.cmd[[colorscheme nord]] -- Theme
--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -15,17 +15,17 @@ keymap('', 'n', 'nzz', {})
 keymap('', 'N', 'Nzz', {})
 
 -- Options to not yank
-keymap('x', '<leader>p', '"_dP', {}) -- Replace without yanking
-keymap('n', '<leader>d', '"_d', {}) -- Delete without yanking'
-keymap('n', '<leader>D', '"_D', {}) -- Delete until EOL without yanking
-keymap('n', '<leader>c', '"_c', {}) -- Ckange without yanking
-keymap('n', '<leader>C', '"_C', {}) -- Change until EOL without yanking
+keymap('x', '<leader>p', '--_dP', {}) -- Replace without yanking
+keymap('n', '<leader>d', '--_d', {}) -- Delete without yanking'
+keymap('n', '<leader>D', '--_D', {}) -- Delete until EOL without yanking
+keymap('n', '<leader>c', '--_c', {}) -- Ckange without yanking
+keymap('n', '<leader>C', '--_C', {}) -- Change until EOL without yanking
 
 -- Yank to clipboard
-keymap('', '<leader>y', '"+y', {}) -- Yank to os clipboard
-keymap('', '<leader>Y', '"+y$', {}) -- Yank until EOL to clipboard
-keymap('n', '<leader>p', '"+p', {}) -- Paste after cursor from clipboard
-keymap('n', '<leader>P', '"+P', {}) -- Paste before cursor from clipboard
+keymap('', '<leader>y', '--+y', {}) -- Yank to os clipboard
+keymap('', '<leader>Y', '--+y$', {}) -- Yank until EOL to clipboard
+keymap('n', '<leader>p', '--+p', {}) -- Paste after cursor from clipboard
+keymap('n', '<leader>P', '--+P', {}) -- Paste before cursor from clipboard
 
 -- Split keybinds
 keymap({'n', 't'}, '<C-h>', '<C-w>h')
@@ -44,3 +44,41 @@ keymap('n', '<leader>ff', builtin.find_files, {})
 keymap('n', '<leader>fg', builtin.live_grep, {})
 keymap('n', '<leader>fb', builtin.buffers, {})
 keymap('n', '<leader>fh', builtin.help_tags, {})
+
+-- Barbar keybinds
+-- Move to previous/next
+keymap('n', '<A-,>', '<Cmd>BufferPrevious<CR>', opts)
+keymap('n', '<A-.>', '<Cmd>BufferNext<CR>', opts)
+-- Re-order to previous/next
+keymap('n', '<A-<>', '<Cmd>BufferMovePrevious<CR>', opts)
+keymap('n', '<A->>', '<Cmd>BufferMoveNext<CR>', opts)
+-- Goto buffer in position...
+keymap('n', '<A-1>', '<Cmd>BufferGoto 1<CR>', opts)
+keymap('n', '<A-2>', '<Cmd>BufferGoto 2<CR>', opts)
+keymap('n', '<A-3>', '<Cmd>BufferGoto 3<CR>', opts)
+keymap('n', '<A-4>', '<Cmd>BufferGoto 4<CR>', opts)
+keymap('n', '<A-5>', '<Cmd>BufferGoto 5<CR>', opts)
+keymap('n', '<A-6>', '<Cmd>BufferGoto 6<CR>', opts)
+keymap('n', '<A-7>', '<Cmd>BufferGoto 7<CR>', opts)
+keymap('n', '<A-8>', '<Cmd>BufferGoto 8<CR>', opts)
+keymap('n', '<A-9>', '<Cmd>BufferGoto 9<CR>', opts)
+keymap('n', '<A-0>', '<Cmd>BufferLast<CR>', opts)
+-- Pin/unpin buffer
+keymap('n', '<A-p>', '<Cmd>BufferPin<CR>', opts)
+-- Close buffer
+keymap('n', '<A-c>', '<Cmd>BufferClose<CR>', opts)
+-- Wipeout buffer
+--                 :BufferWipeout
+-- Close commands
+--                 :BufferCloseAllButCurrent
+--                 :BufferCloseAllButPinned
+--                 :BufferCloseAllButCurrentOrPinned
+--                 :BufferCloseBuffersLeft
+--                 :BufferCloseBuffersRight
+-- Magic buffer-picking mode
+keymap('n', '<C-p>', '<Cmd>BufferPick<CR>', opts)
+-- Sort automatically by...
+keymap('n', '<Space>bb', '<Cmd>BufferOrderByBufferNumber<CR>', opts)
+keymap('n', '<Space>bd', '<Cmd>BufferOrderByDirectory<CR>', opts)
+keymap('n', '<Space>bl', '<Cmd>BufferOrderByLanguage<CR>', opts)
+keymap('n', '<Space>bw', '<Cmd>BufferOrderByWindowNumber<CR>', opts)
