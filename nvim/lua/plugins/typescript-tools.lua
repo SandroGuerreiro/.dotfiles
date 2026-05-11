9508973414e33ea1aa8281285649ec55c108c596
Sandro Guerreiro <sandrocguerreiro@gmail.com>
2026-05-11 12:58:25 +0200

    Fix definitions commands

--- nvim/lua/plugins/typescript-tools.lua
+++ nvim/lua/plugins/typescript-tools.lua
@@ -2,5 +2,9 @@ return {
   "pmizio/typescript-tools.nvim",
   dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
   ft = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
-  opts = {},
+  opts = {
+    on_attach = function(_, bufnr)
+      vim.keymap.set('n', 'gd', '<cmd>TSToolsGoToSourceDefinition<cr>', { buffer = bufnr })
+    end,
+  },
 }
